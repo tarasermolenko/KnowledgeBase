5308cc3fe14f99427644ec652ecec7707a8b5145
tarasermolenko <49844905+tarasermolenko@users.noreply.github.com>
2021-09-04 19:32:21 -0700

    Add files via upload

--- longestsubstringwithoutrepeating.cpp
+++ longestsubstringwithoutrepeating.cpp
@@ -1,51 +1,57 @@
-// empty case 
-if (s.length() == 0)
-{
-    return 0;           
-}
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) 
+    {
+        // empty case 
+        if (s.length() == 0)
+        {
+            return 0;           
+        }
 
-// single case
-if (s.length() == 1)
-{
-    return 1;           
-}
+        // single case
+	    if (s.length() == 1)
+        {
+            return 1;           
+        }
 
-int longestSub = 0;
-int start = 0;
-int i = 0;
+		int longestSub = 0;
+		int start = 0;
+		int i = 0;
 
-while (i < s.length())
-{
-	bool repeating = false;
-    int j = start;
-
-	for (j = start; j < i; j++)
-	{
-		if (s[j] == s[i])
+        while (i < s.length())
 		{
-			repeating = true;
-			break;
-		}
-	}
+			bool repeating = false;
+		    int j = start;
 
-	if (!repeating)
-	{
-		i++;
-	}
-	else
-	{
+			for (j = start; j < i; j++)
+			{
+				if (s[j] == s[i])
+				{
+					repeating = true;
+					break;
+				}
+			}
+
+			if (!repeating)
+			{
+				i++;
+			}
+			else
+			{
+				if(longestSub < i - start)
+                {
+                    longestSub = i - start;
+                }
+                
+				start = j + 1;
+			}
+		}
+
 		if(longestSub < i - start)
         {
             longestSub = i - start;
         }
-        
-		start = j + 1;
-	}
-}
-
-if(longestSub < i - start)
-{
-    longestSub = i - start;
-}
-        
-return longestSub;
+                
+		return longestSub;
+    }
+};
